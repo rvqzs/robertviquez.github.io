cf547ecb6266abbb013fb9db09ff5aef8a75f9df
rviquez <146051750+rvqzs@users.noreply.github.com>
2024-08-04 18:52:32 -0600

    Responsiveness

--- js/index.js
+++
@@ -1,9 +0,0 @@
-    document.addEventListener("DOMContentLoaded", function() {
-        const inactiveLinks = document.querySelectorAll('.nav-link.inactive');
-        inactiveLinks.forEach(function(link) {
-            link.addEventListener('click', function(event) {
-                event.preventDefault();
-            });
-        });
-    });
-
